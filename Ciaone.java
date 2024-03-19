faedd78616edd8f271ae053dfce0bafede331308
dottorscotti <davide.nunin@gmail.com>
2024-03-19 10:35:38 +0100

    ciaone

---
+++ Ciaone.java
@@ -0,0 +1,14 @@
+public class Ciaone {
+    public static void main(String args[]){
+        System.out.println("ciaone");
+        System.out.println(Ciaone.fib(3));
+    }
+    private static int fib(int n){
+        if (n==1 || n==0){
+            return 1;
+        }
+        else{
+            return Ciaone.fib(n-1)+Ciaone.fib(n-2);
+        }
+    }
+}
